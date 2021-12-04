418c872eb8a07fc43fc5811ff26e51ba88e04919
Jackie Liu <jackolake@gmail.com>
2021-12-04 16:50:40 +0800

    day 4

--- day_4.py
+++ day_4.py
@@ -10,16 +10,16 @@ if __name__ == '__main__':
         row = 0
         for line in txt.readlines():
             if ',' in line:
-                draws = [int(c) for c in line.strip().split(',')]
+                draws = [int(c) for c in line.strip().split(',')]  # First line
             elif len(line) > 1:
                 if row == 0:
                     board_coordinates = {}
-                nums = [int(c) for c in line.strip().replace('\n', '').split(' ') if c]
+                nums = [int(c) for c in line.strip().replace('\n', '').split(' ') if c]  # one row
                 for col in range(max_col):
-                    board_coordinates[nums[col]] = (row, col)
+                    board_coordinates[nums[col]] = (row, col)  # {number => (x, y) coordinates}
                 row = (row + 1) % max_row
                 if row == 0:
-                    boards.append(board_coordinates)
+                    boards.append(board_coordinates)  # Got 5 rows, save the board
 
     def get_win_sequence():
         board_occupancy = [pandas.DataFrame(numpy.zeros((max_row, max_col))) for _ in boards]
@@ -29,24 +29,20 @@ if __name__ == '__main__':
                 if not any([(b[0] == board_num) for b in won_boards]):  # process if not won yet
                     # Make a move in the board
                     draw_row, draw_col = board.get(draw, (None, None))
-                    if draw_row is not None and draw_col is not None:
+                    if (draw_row, draw_col) != (None, None):
                         board_occupancy[board_num][draw_row][draw_col] = 1
                         # Check winning condition (row or column)
                         if any([board_occupancy[board_num].iloc[i].sum() == max_row for i in range(max_row)]) \
                                 or any([board_occupancy[board_num][j].sum() == max_col for j in range(max_col)]):
-                            # get inactive numbers
-                            counter = 0
-                            for d, coord in board.items():
-                                r, c = coord
-                                if board_occupancy[board_num][r][c] == 0:
-                                    counter += d
-                            won_boards.append((board_num, draw*counter))
-                            continue
+                            # get unmarked numbers
+                            unmarked = sum([d for d, coord in board.items()
+                                            if board_occupancy[board_num][coord[0]][coord[1]] == 0])
+                            won_boards.append((board_num, draw*unmarked))
         return won_boards
 
     # Show results
     results = get_win_sequence()
     # Part 1
-    print(results[0])
+    print(results[0])  # 58838
     # Part 2
-    print(results[-1])
+    print(results[-1])  # 6256
