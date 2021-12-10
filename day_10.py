5a0ab7e6671675d6c0596305294412228719b195
Jackie Liu <jackolake@gmail.com>
2021-12-11 01:11:08 +0800

    day 10

--- day_10.py
+++ day_10.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 open_to_close = {'(': ')', '[': ']', '{': '}', '<': '>'}
 close_to_open = dict((v, k) for k, v in open_to_close.items())
 
@@ -15,25 +17,17 @@ if __name__ == '__main__':
     incompletes = []
     for line in lines:
         stack = []
-        corrupt = None
         for i, c in enumerate(line):
             if c in close_to_open.values():  # opening bracket
                 stack.append(c)
             elif stack and stack.pop() != close_to_open[c]:  # Closing bracket with corruption
-                corrupt = c
+                corrupts.append(c)
                 break
-            if stack and (i == len(line) - 1):  # finished processing but incomplete
+            if (i == len(line) - 1) and stack:  # finished processing this line but incomplete
                 incompletes.append([open_to_close[c] for c in reversed(stack)])
-        if corrupt:
-            corrupts.append(corrupt)
-    # Part 1
+    # Part 1: calculate sum of corrupt scores
     print(sum([corrupt_scores[c] for c in corrupts]))  # 315693
 
-    # Part 2
-    part2 = []
-    for incomplete in incompletes:
-        score = 0
-        for c in incomplete:
-            score = 5 * score + incomplete_scores[c]
-        part2.append(score)
-    print(sorted(part2)[int(len(incompletes)/2)])  # 1870887234
+    # Part 2: Calculate incomplete scores and get the median
+    part2 = [reduce(lambda n1, n2: 5*n1 + n2, [incomplete_scores[c] for c in incomplete]) for incomplete in incompletes]
+    print(sorted(part2)[int(len(part2)/2)])  # 1870887234
