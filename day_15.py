7e0246fa9fd97b51fb56e25223c6dac031106147
Jackie Liu <jackolake@gmail.com>
2021-12-15 23:15:44 +0800

    day 15 - add comment

--- day_15.py
+++ day_15.py
@@ -32,16 +32,17 @@ if __name__ == '__main__':
 
     # Explore
     while explorer_heap:
-        explorer_risk, explorer_node = heapq.heappop(explorer_heap)  # least cost
+        explorer_risk, explorer_node = heapq.heappop(explorer_heap)  # Explore unvisited node closest to start
         if explorer_node not in visited_nodes:
-            visited_nodes.add(explorer_node)
+            visited_nodes.add(explorer_node)  # Once visited, risk[explorer_node] cannot go smaller
             if explorer_node == end:  # early terminating condition
                 break
             for mod_row, mod_col in [(1, 0), (0, 1), (-1, 0), (0, -1)]:  # Down, Right, Up, Left
                 test_coord = (explorer_node[0] + mod_row, explorer_node[1] + mod_col)
-                if 0 <= test_coord[0] < risk.shape[0] and 0 <= test_coord[1] < risk.shape[1]:
-                    if explorer_risk + risk.iloc[test_coord] < risk_from_start[test_coord]:
-                        risk_from_start[test_coord] = explorer_risk + risk.iloc[test_coord]
-                    if test_coord not in visited_nodes:
+                if 0 <= test_coord[0] < risk.shape[0] and 0 <= test_coord[1] < risk.shape[1]:  # Map bound
+                    test_new_risk = explorer_risk + risk.iloc[test_coord]
+                    if test_new_risk < risk_from_start[test_coord]:  # New smaller risk sum found, Save the result
+                        risk_from_start[test_coord] = test_new_risk  # optionally: save prev[test_coord] = explorer_node
+                    if test_coord not in visited_nodes:  # Mark unexplored neighbours for exploration later
                         heapq.heappush(explorer_heap, (risk_from_start[test_coord], test_coord))
     print(risk_from_start[end])  # part1: 824, part2: 3063
