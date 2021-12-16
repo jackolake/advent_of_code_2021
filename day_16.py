6e04e08f4c72a2ab9855de946d5516e319ace5e9
Jackie Liu <jackolake@gmail.com>
2021-12-17 00:02:00 +0800

    day 16 - clean up

--- day_16.py
+++ day_16.py
@@ -2,7 +2,7 @@ import math
 from functools import reduce
 
 
-class Node(object):
+class Node(object):  # Tree node
     def __init__(self, code, version, type, parent=None, childrens=[], value=0):
         self.code = code
         self.version = version
@@ -17,6 +17,40 @@ def hex_to_bin_packet(s):
     return bin(int(s, 16))[2:].zfill(len(s) * 4)
 
 
+def read_packet(s, parent, message_limit=math.inf):  # substring, parent node, message_limit
+    message_parsed = 0
+    while message_parsed < message_limit and len(s) >= 6 and int(s, 2) != 0:
+        packet_version = int(s[:3], 2)
+        packet_type_id = int(s[3:6], 2)
+        if packet_type_id == 4:  # literal value
+            literal_string = s[6:]
+            value_string = ''
+            for i in range(int(math.ceil(len(literal_string) / 5))):
+                current_bits = literal_string[i * 5: i * 5 + 5]  # Load 5 bits
+                value_string = value_string + current_bits[1:]
+                if current_bits[0] == '0':  # Stop after last bit
+                    s = literal_string[(i + 1) * 5:]
+                    break
+            value = int(value_string, 2)
+            node = Node(code=value_string, version=packet_version,
+                        type=packet_type_id, parent=parent,
+                        childrens=[], value=value)
+        else:  # operator packet
+            length_type_id = s[6]
+            node = Node(code=s, version=packet_version, type=packet_type_id, parent=parent, childrens=[], value=0)
+            if length_type_id == '0':  # next 15 bits = total length in bits of the sub-packets
+                subpackets_length = int(s[7: 22], 2)
+                read_packet(s[22:22 + subpackets_length], parent=node)
+                s = s[22 + subpackets_length:]  # get rid of substring which is processed
+            else:  # next 11 bits = number of sub-packets immediately contained
+                num_of_packets = int(s[7: 18], 2)
+                s = read_packet(s[18:], parent=node, message_limit=num_of_packets)
+        # Increment message counter
+        parent.childrens.append(node)
+        message_parsed += 1
+    return s
+
+
 if __name__ == '__main__':
     # inputs
     with open('inputs/day_16.txt', 'r') as txt:
@@ -24,44 +58,11 @@ if __name__ == '__main__':
     bin_packet = hex_to_bin_packet(hex_input)
     root = Node(code=bin_packet, version=0, type=None, parent=None, childrens=[])
 
-    # Define method
-    def read_packet(s, parent, message_limit=math.inf):
-        message_parsed = 0
-        while message_parsed < message_limit and len(s) >= 6 and int(s, 2) != 0:
-            packet_version = int(s[:3], 2)
-            packet_type_id = int(s[3:6], 2)
-            if packet_type_id == 4:  # literal value
-                literal_string = s[6:]
-                value_string = ''
-                for i in range(int(math.ceil(len(literal_string) / 5))):
-                    current_bits = literal_string[i * 5: i * 5 + 5]  # Load 5 bits
-                    value_string = value_string + current_bits[1:]
-                    if current_bits[0] == '0':  # Stop after last bit
-                        s = literal_string[(i + 1) * 5:]
-                        break
-                value = int(value_string, 2)
-                node = Node(code=value_string, version=packet_version,
-                            type=packet_type_id, parent=parent,
-                            childrens=[], value=value)
-            else:  # operator packet
-                length_type_id = s[6]
-                node = Node(code=s, version=packet_version, type=packet_type_id, parent=parent, childrens=[], value=0)
-                if length_type_id == '0':  # next 15 bits = total length in bits of the sub-packets
-                    subpackets_length = int(s[7: 22], 2)
-                    read_packet(s[22:22 + subpackets_length], parent=node)
-                    s = s[22 + subpackets_length:]  # get rid of substring which is processed
-                else:  # next 11 bits = number of sub-packets immediately contained
-                    num_of_packets = int(s[7: 18], 2)
-                    s = read_packet(s[18:], parent=node, message_limit=num_of_packets)
-            # Increment message counter
-            parent.childrens.append(node)
-            message_parsed += 1
-        return s
-
-    # Part 1
+    # Parse input into tree
     read_packet(bin_packet, parent=root)
     root = root.childrens[0]  # Skip the dummy root
 
+    # Tree traversal function
     def traverse(node, part1):
         if node.type == 4:  # Literal node
             return node.version if part1 else node.value
